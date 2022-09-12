5b4139cbaa4a9eaae53a34f15da0f00cacd22cc1
HasanBakhtiar <hasan.doctype@gmail.com>
2022-09-12 20:59:09 +0400

    js start

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,26 @@
+
+// document.write("Hello World");
+// one line comment
+
+/**
+ * more 
+ * line 
+ * comment
+ */
+
+
+// console.log("Hello");
+// console.log("Hello World");
+
+// console.log("Hello1");
+// console.log("Hello2");
+// debugger;
+// consolae.log("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+
+
+
+
+
+
---
+++ JavaScript/lesson2-dataTypes/js/script.js
@@ -0,0 +1,54 @@
+// var info = "Hello World";
+// console.log(info);
+
+// DATA TYPES
+// Basic data types
+// string
+// const text1  = 'Hello1';
+// const text2  = "Hello2";
+
+// console.log(text2+text1);
+// -------------------------------------
+// number
+// const mynumb1 = "5";
+// const mynumb2 = 10;
+// console.log(mynumb1+mynumb2);
+
+// -------------------------------------
+// boolean
+// true 
+// false
+// if (true) {
+//     console.log('this script is perfect');
+// }else{
+//     console.log('this script is wrong!');
+// }
+
+// -------------------------------------
+
+
+// Composite data types
+// Array
+// const infoArr = ['Hello',15,true,'js'];
+// console.log(infoArr);
+// -------------------------------------
+
+// Object
+// const infoObj = {
+//     brand:"BMW",
+//     color: "yellow",
+//     year:2022,
+//     new: true
+
+// }
+// console.log(infoObj);
+
+// Special data types
+
+// undefined
+// const ab = undefined;
+// console.log(ab);
+// -------------------------------------
+// null 
+// const a = null;
+// console.log(a);
