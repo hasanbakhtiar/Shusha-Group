a9b2ff2cc4c5f5862e76e676152ba992a99e5c38
HasanBakhtiar <hasan.doctype@gmail.com>
2022-09-19 20:59:06 +0400

    js

---
+++ JavaScript/lesson3-operators/js/script.js
@@ -0,0 +1,133 @@
+// var infoOne = 5;
+// var infoTwo = 15;
+// infoOne **= infoTwo;
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+// var infoOne = 10;
+// var infoTwo = 2;
+// var infoThree = infoOne % infoTwo;
+// infoThree++;
+// Arithmetic operators
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+
+// console.log(infoThree);
+// ====================================================
+
+
+// Comparison Operators
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+
+// var infoOne = "10";
+// var infoTwo = 10;
+
+// if(infoOne !== infoTwo){
+//     console.log('this is true');
+// }else{
+//     console.log('this is false');
+
+// }
+
+// LOGICAL operators 
+
+// || - OR
+// && - AND
+// !  - NOT
+
+
+// if (true || false || false || false) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+
+// }
+
+// if (!false || 10>10 || false || false) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+
+// }
+
+
+// string operators
+// +
+// +=
+// var a = "Java";
+// var b = "Script";
+// a+=b;
+// console.log(a);
+
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a =5, b = 10;
+// console.log(a,b);
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+// var a = false;
+// console.log(typeof a);
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)(ternary)
+// 5>10?console.log('true'): console.log('false');
+
+// 5>10 ? console.log(true): console.log(false);
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// var a = 5;
+// console.log(void(a));
+// delete       (objectin dəyərini silmək üçündür)
+// var a = {
+//     name:'shusha'
+// }
+// delete a.name
+// console.log(a.name);
+
+// new          (yeni dəyər yaratməq üçündür)
+// this         (mövcud dəyəri işarələmək üçündür)
+// class Car{
+//     constructor(name,surname){
+//         this.iname = name;
+//         this.isurname = surname;
+//     }
+
+//     sayHi(){
+//         return `Hi, ${this.iname + this.isurname}`
+//     }
+// }
+// const myCar = new Car('Emil ',"Rehimov");
+// console.log(myCar.sayHi());
+// in           (dəyişkənin(object) daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+
+// instanceof   (dəyişkənin(objectin xüsusiyyətinin) içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
