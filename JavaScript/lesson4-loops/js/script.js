9caa0cdfeccab188a143c486e51eae47922a9cb9
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-09-27 11:04:04 +0400

    loops

---
+++ JavaScript/lesson4-loops/js/script.js
@@ -0,0 +1,72 @@
+// var a = 1;
+
+// while(a<=10){
+//     console.log(a);
+//     // a++;
+//     a+=3;
+// }
+
+// ============================================================
+// var b = 1;
+
+// do{
+//     console.log(b);
+//     b++;
+// }while(b>=10);
+
+// ====================================================================== 
+// for(let c = 1; c<10;c++){
+//     console.log("ES"+c);
+// }
+
+// ================================================================
+
+// for(let c = 1; c<10;c++){
+//     if (c ===5) {
+//         // break;
+//         continue;
+//     }
+    
+//     console.log("ES"+c);
+// }
+// =================================================================
+
+// const infoArr = ['table','pen','book'];
+// for( x in infoArr){
+//     console.log(infoArr[x]+x);
+// }
+
+const infoArr = ['table','pen','book'];
+for( x of infoArr){
+    console.log(x);
+}
+
+
+
+// const infoArr = ['table','pen','book'];
+// for(let count = 0; count <3; count++){
+//     console.log(infoArr[count]);
+// }
+
+// Arrays
+// const infoArr = ['table','pen','book'];
+// console.log(...infoArr);
+// console.log(infoArr[1]);
+
+// const infoArr = [];
+// infoArr[0] = 'pen';
+// infoArr[1] = 'book';
+// infoArr[2] = 'table';
+// const infoArr = ['table','pen','book'];
+// infoArr[2] = 'table';
+// console.log(infoArr);
+
+// const infoArr = ['table','pen','book'];
+// infoArr['hello'] = 'Hello everyone';
+// console.log(infoArr['hello']);
+
+
+// const infoArr = ['pen',[1,2,3,['book','pen',[true,'new value',['hello']],'snow'],4,5],'rain','sunny'];
+// console.log(infoArr[1][3][2][2][0]);
+
+
